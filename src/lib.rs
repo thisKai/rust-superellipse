bb4c780724759937cbf298940967f8e297bcf4f0
Kai Patrick <thiskaiguy@gmail.com>
2024-07-02 12:30:06 +0100

    Port rounded rect and xywh functions to work without skia

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub fn superellipse_points(
     center_y: f32,
     radius_x: f32,
     radius_y: f32,
-) -> impl Iterator<Item = (f32, f32)> {
+) -> impl Iterator<Item = Point> {
     angles_in_degrees
         .map(|degrees| PI * degrees as f32 / 180.0)
         .map(move |radians| {
@@ -20,6 +20,102 @@ pub fn superellipse_points(
             let y =
                 center_y + (radians.sin().abs().powf(2.0 / n) * radius_y * radians.sin().signum());
 
-            (x, y)
+            Point { x, y }
         })
+}
+
+pub fn superellipse_points_xywh(
+    angles_in_degrees: impl Iterator<Item = u32>,
+    n: f32,
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+) -> impl Iterator<Item = Point> {
+    let center_x = x + (width / 2.0);
+    let center_y = y + (height / 2.0);
+
+    let radius_x = width / 2.0;
+    let radius_y = height / 2.0;
+
+    superellipse_points(angles_in_degrees, n, center_x, center_y, radius_x, radius_y)
+}
+
+pub fn superellipse_rounded_rect_points(
+    n: f32,
+    rect: Rect,
+    corner_radii: CornerRadii,
+) -> impl Iterator<Item = Point> {
+    let radius_bottom_right = corner_radii.bottom_right;
+    let radius_bottom_left = corner_radii.bottom_left;
+    let radius_top_left = corner_radii.top_left;
+    let radius_top_right = corner_radii.top_right;
+
+    let bottom_right_center_x = rect.right - radius_bottom_right.x;
+    let bottom_right_center_y = rect.bottom - radius_bottom_right.y;
+    let bottom_right_points = superellipse_points(
+        0..=90,
+        n,
+        bottom_right_center_x,
+        bottom_right_center_y,
+        radius_bottom_right.x,
+        radius_bottom_right.y,
+    );
+
+    let bottom_left_center_x = rect.left + radius_bottom_left.x;
+    let bottom_left_center_y = rect.bottom - radius_bottom_left.y;
+    let bottom_left_points = superellipse_points(
+        90..=180,
+        n,
+        bottom_left_center_x,
+        bottom_left_center_y,
+        radius_bottom_left.x,
+        radius_bottom_left.y,
+    );
+
+    let top_left_center_x = rect.left + radius_top_left.x;
+    let top_left_center_y = rect.top + radius_top_left.y;
+    let top_left_points = superellipse_points(
+        180..=270,
+        n,
+        top_left_center_x,
+        top_left_center_y,
+        radius_top_left.x,
+        radius_top_left.y,
+    );
+
+    let top_right_center_x = rect.right - radius_top_right.x;
+    let top_right_center_y = rect.top + radius_top_right.y;
+    let top_right_points = superellipse_points(
+        270..=360,
+        n,
+        top_right_center_x,
+        top_right_center_y,
+        radius_top_right.x,
+        radius_top_right.y,
+    );
+
+    bottom_right_points
+        .chain(bottom_left_points)
+        .chain(top_left_points)
+        .chain(top_right_points)
+}
+
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+}
+
+pub struct Rect {
+    pub left: f32,
+    pub right: f32,
+    pub top: f32,
+    pub bottom: f32,
+}
+
+pub struct CornerRadii {
+    pub top_left: Point,
+    pub top_right: Point,
+    pub bottom_left: Point,
+    pub bottom_right: Point,
 }
