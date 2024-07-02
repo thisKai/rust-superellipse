6e33702c88b9db8bbc98d2674af2f4b8da01df0a
Kai Patrick <thiskaiguy@gmail.com>
2024-07-02 12:45:59 +0100

    Refactor args with points and rects

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,11 @@ pub mod partial;
 pub mod skia;
 
 /// if n == 2.0 this draws a circle
-pub fn superellipse_points(
-    n: f32,
-    center_x: f32,
-    center_y: f32,
-    radius_x: f32,
-    radius_y: f32,
-) -> impl Iterator<Item = Point> {
-    partial::superellipse_points(0..360, n, center_x, center_y, radius_x, radius_y)
+pub fn superellipse_points(n: f32, center: Point, radius: Point) -> impl Iterator<Item = Point> {
+    partial::superellipse_points(0..360, n, center, radius)
 }
-pub fn superellipse_points_xywh(
-    n: f32,
-    x: f32,
-    y: f32,
-    width: f32,
-    height: f32,
-) -> impl Iterator<Item = Point> {
-    partial::superellipse_points_xywh(0..360, n, x, y, width, height)
+pub fn superellipse_points_rect(n: f32, rect: Rect) -> impl Iterator<Item = Point> {
+    partial::superellipse_points_rect(0..360, n, rect)
 }
 
 pub fn superellipse_rounded_rect_points(
@@ -33,49 +21,33 @@ pub fn superellipse_rounded_rect_points(
     let radius_top_left = corner_radii.top_left;
     let radius_top_right = corner_radii.top_right;
 
-    let bottom_right_center_x = rect.right - radius_bottom_right.x;
-    let bottom_right_center_y = rect.bottom - radius_bottom_right.y;
-    let bottom_right_points = partial::superellipse_points(
-        0..=90,
-        n,
-        bottom_right_center_x,
-        bottom_right_center_y,
-        radius_bottom_right.x,
-        radius_bottom_right.y,
-    );
+    let bottom_right_center = Point {
+        x: rect.right - radius_bottom_right.x,
+        y: rect.bottom - radius_bottom_right.y,
+    };
+    let bottom_right_points =
+        partial::superellipse_points(0..=90, n, bottom_right_center, radius_bottom_right);
 
-    let bottom_left_center_x = rect.left + radius_bottom_left.x;
-    let bottom_left_center_y = rect.bottom - radius_bottom_left.y;
-    let bottom_left_points = partial::superellipse_points(
-        90..=180,
-        n,
-        bottom_left_center_x,
-        bottom_left_center_y,
-        radius_bottom_left.x,
-        radius_bottom_left.y,
-    );
+    let bottom_left_center = Point {
+        x: rect.left + radius_bottom_left.x,
+        y: rect.bottom - radius_bottom_left.y,
+    };
+    let bottom_left_points =
+        partial::superellipse_points(90..=180, n, bottom_left_center, radius_bottom_left);
 
-    let top_left_center_x = rect.left + radius_top_left.x;
-    let top_left_center_y = rect.top + radius_top_left.y;
-    let top_left_points = partial::superellipse_points(
-        180..=270,
-        n,
-        top_left_center_x,
-        top_left_center_y,
-        radius_top_left.x,
-        radius_top_left.y,
-    );
+    let top_left_center = Point {
+        x: rect.left + radius_top_left.x,
+        y: rect.top + radius_top_left.y,
+    };
+    let top_left_points =
+        partial::superellipse_points(180..=270, n, top_left_center, radius_top_left);
 
-    let top_right_center_x = rect.right - radius_top_right.x;
-    let top_right_center_y = rect.top + radius_top_right.y;
-    let top_right_points = partial::superellipse_points(
-        270..=360,
-        n,
-        top_right_center_x,
-        top_right_center_y,
-        radius_top_right.x,
-        radius_top_right.y,
-    );
+    let top_right_center = Point {
+        x: rect.right - radius_top_right.x,
+        y: rect.top + radius_top_right.y,
+    };
+    let top_right_points =
+        partial::superellipse_points(270..=360, n, top_right_center, radius_top_right);
 
     bottom_right_points
         .chain(bottom_left_points)
@@ -93,6 +65,14 @@ pub struct Rect {
     pub right: f32,
     pub top: f32,
     pub bottom: f32,
+}
+impl Rect {
+    pub fn width(&self) -> f32 {
+        self.right - self.left
+    }
+    pub fn height(&self) -> f32 {
+        self.bottom - self.top
+    }
 }
 
 pub struct CornerRadii {
--- src/partial.rs
+++ src/partial.rs
@@ -1,40 +1,41 @@
 use std::f32::consts::PI;
 
-use crate::Point;
+use crate::{Point, Rect};
 
 pub fn superellipse_points(
     angles_in_degrees: impl Iterator<Item = u32>,
     n: f32,
-    center_x: f32,
-    center_y: f32,
-    radius_x: f32,
-    radius_y: f32,
+    center: Point,
+    radius: Point,
 ) -> impl Iterator<Item = Point> {
     angles_in_degrees
         .map(|degrees| PI * degrees as f32 / 180.0)
         .map(move |radians| {
             let x =
-                center_x + (radians.cos().abs().powf(2.0 / n) * radius_x * radians.cos().signum());
+                center.x + (radians.cos().abs().powf(2.0 / n) * radius.x * radians.cos().signum());
             let y =
-                center_y + (radians.sin().abs().powf(2.0 / n) * radius_y * radians.sin().signum());
+                center.y + (radians.sin().abs().powf(2.0 / n) * radius.y * radians.sin().signum());
 
             Point { x, y }
         })
 }
 
-pub fn superellipse_points_xywh(
+pub fn superellipse_points_rect(
     angles_in_degrees: impl Iterator<Item = u32>,
     n: f32,
-    x: f32,
-    y: f32,
-    width: f32,
-    height: f32,
+    rect: Rect,
 ) -> impl Iterator<Item = Point> {
-    let center_x = x + (width / 2.0);
-    let center_y = y + (height / 2.0);
+    let width = rect.width();
+    let height = rect.height();
 
-    let radius_x = width / 2.0;
-    let radius_y = height / 2.0;
+    let center = Point {
+        x: rect.left + (width / 2.0),
+        y: rect.top + (height / 2.0),
+    };
+    let radius = Point {
+        x: width / 2.0,
+        y: height / 2.0,
+    };
 
-    superellipse_points(angles_in_degrees, n, center_x, center_y, radius_x, radius_y)
+    superellipse_points(angles_in_degrees, n, center, radius)
 }
